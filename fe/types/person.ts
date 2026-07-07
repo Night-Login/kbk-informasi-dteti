0148d4ca0cf421d665edcd675d18fb431b297bd8
SentientCorn <radityanarotama0@gmail.com>
2026-07-07 22:23:48 +0700

    feat: added person types

---
+++ fe/types/person.ts
@@ -0,0 +1,32 @@
+
+export interface PersonLite {
+  
+  // TODO: Update types after the backend API is ready
+  id: string; 
+  fullName: string;
+  position: string;
+  isSupervisorAvailable: boolean;
+  profilePictureUrl?: string;
+
+  contact: {
+    labName: string;
+    phone: string;
+    email: string;
+  };
+}
+
+export interface PersonFull extends PersonLite {
+
+  // TODO: Update types after the backend API is ready
+  shortBio: string;
+  degrees: string[];
+  researchAreas: string[];
+  teachingAssistants: string[];
+  advisees: string[];
+
+  academicLinks: {
+    sinta: string;
+    scopus: string;
+    scholar: string;
+  };
+}
