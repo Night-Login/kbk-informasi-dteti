51f8d024b9f42a4515fb0a7923c924042b065caf
SentientCorn <radityanarotama0@gmail.com>
2026-07-09 09:54:01 +0700

    feat: improved person type

--- fe/types/person.ts
+++ fe/types/person.ts
@@ -1,8 +1,25 @@
+export interface teachingAssistant {
+  fullName: string;
+  profilePictureUrl?: string;
+
+  contact: {
+    phone: string;
+    email: string;
+  };
+}
+
+export interface publication {
+  title: string;
+  link: string;
+  type: string;
+  date: string;
+  peopleInvolved: string[];
+  tags: string[];
+}
 
 export interface PersonLite {
-  
   // TODO: Update types after the backend API is ready
-  id: string; 
+  id: string;
   fullName: string;
   position: string;
   isSupervisorAvailable: boolean;
@@ -16,13 +33,12 @@ export interface PersonLite {
 }
 
 export interface PersonFull extends PersonLite {
-
   // TODO: Update types after the backend API is ready
   shortBio: string;
   longBio: string;
   degrees: string[];
   researchAreas: string[];
-  teachingAssistants: string[];
+  teachingAssistants: teachingAssistant[];
   advisees: string[];
 
   academicLinks: {
@@ -30,4 +46,7 @@ export interface PersonFull extends PersonLite {
     scopus: string;
     scholar: string;
   };
+
+  publications: publication[];
+  awards: string[];
 }
