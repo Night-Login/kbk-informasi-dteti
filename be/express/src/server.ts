b9b2c2d3ba8dde8478b6b8f4674c9ca89d6db643
SentientCorn <radityanarotama0@gmail.com>
2026-07-20 20:22:33 +0700

    feat: added essential middleware libraries

--- be/express/src/server.ts
+++ be/express/src/server.ts
@@ -1,11 +1,28 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+// Security Middleware
+app.use(helmet());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true
+}));
+app.use(compression());
+app.use(morgan('dev'));
+
+// Body Parsing
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
     res.json({
